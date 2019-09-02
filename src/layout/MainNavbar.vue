d677df7dc8fd9480cbd0e318739fb46cb73cc0a0
memokun <510054465@qq.com>
2019-09-02 16:34:35 +0800

    顶部导航修改、login页面

--- src/layout/MainNavbar.vue
+++ src/layout/MainNavbar.vue
@@ -1,16 +1,17 @@
 <template>
   <el-menu :default-active="activeIndex" class="navmenu" mode="horizontal" @select="handleSelect">
-    <el-submenu index="5" style="float:right">
+    <el-submenu index="6" style="float:right">
       <template slot="title">Settings</template>
-      <el-menu-item index="5-1"><router-link to="/expertiseCommunity">Fellow Prime</router-link></el-menu-item>
-      <el-menu-item index="5-2"><router-link to="/expertiseCommunity">个人信息</router-link></el-menu-item>
-      <el-menu-item index="5-3"><router-link to="/expertiseCommunity">账号密码</router-link></el-menu-item>
-      <el-menu-item index="5-4"><router-link to="/expertiseCommunity">系统设置</router-link></el-menu-item>
+      <el-menu-item index="6-1"><router-link to="/expertiseCommunity" class="router-link-active">Fellow Prime</router-link></el-menu-item>
+      <el-menu-item index="6-2"><router-link to="/expertiseCommunity" class="router-link-active">个人信息</router-link></el-menu-item>
+      <el-menu-item index="6-3"><router-link to="/expertiseCommunity" class="router-link-active">账号密码</router-link></el-menu-item>
+      <el-menu-item index="6-4"><router-link to="/expertiseCommunity" class="router-link-active">系统设置</router-link></el-menu-item>
     </el-submenu>
-    <el-menu-item index="4" style="float:right"><router-link to="/receivedMessages">Messages</router-link></el-menu-item>
-    <el-menu-item index="3" style="float:right"><router-link to="/friendsApproved">Friends</router-link></el-menu-item>
-    <el-menu-item index="2" style="float:right"><router-link to="/expertiseCommunity">Expertise Community</router-link></el-menu-item>
-    <el-menu-item index="1" style="float:right"><router-link to="/" >Home</router-link></el-menu-item>
+    <el-menu-item index="5" style="float:right"><router-link to="/receivedMessages" class="router-link-active">Messages</router-link></el-menu-item>
+    <el-menu-item index="4" style="float:right"><router-link to="/friendsApproved" class="router-link-active">Friends</router-link></el-menu-item>
+    <el-menu-item index="3" style="float:right"><router-link to="/expertiseCommunity" class="router-link-active">Expertise Community</router-link></el-menu-item>
+    <el-menu-item index="2" style="float:right"><router-link to="/login" class="router-link-active">Login</router-link></el-menu-item>
+    <el-menu-item index="1" style="float:right"><router-link to="/" class="router-link-active">Home</router-link></el-menu-item>
   </el-menu>
 </template>
 <style >
--- src/views/login/login.vue
+++ src/views/login/login.vue
@@ -1,3 +1,30 @@
 <template>
-<h1>Login</h1>
+<div class="login">
+    <el-form ref="loginForm" v-model="loginForm" label-width="80px">
+        <el-form-item label="账号"></el-form-item>
+    </el-form>
+</div>
 </template>
+
+<script>
+export default {
+  components: {},
+  props: {},
+  data() {
+    return {
+        loginForm:{
+            account:"",
+            password:"",
+        },
+    };
+  },
+  watch: {},
+  computed: {},
+  methods: {},
+  created() {},
+  mounted() {}
+};
+
+<style lang='' scoped>
+
+</style>
