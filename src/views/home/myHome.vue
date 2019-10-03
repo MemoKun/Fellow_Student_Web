a4e9ab776aec2cd62459b45ebae901ce4cc7ef1c
JunhaoXue <junhaoxue@outlook.com>
2019-10-03 16:30:43 +0800

    myHome1.1新增推荐活动、推荐好友、推荐组织

--- src/views/home/myHome.vue
+++ src/views/home/myHome.vue
@@ -6,7 +6,7 @@
           <el-row>
             <el-card style="margin-top:50px;margin-bottom:50px">
               <el-row>
-                <el-col :span="10">
+                <el-col :span="7">
                   <div class="container" style="margin:20px">
                     <div>
                       <el-row>
@@ -61,6 +61,11 @@
                             <el-row>
                               <el-link class="todoContent">
                                 <i class="el-icon-star-off"></i>上传一张你的真人照片
+                              </el-link>
+                            </el-row>
+                            <el-row>
+                              <el-link class="todoContent">
+                                <i class="el-icon-star-off"></i>加入一个理想的组织
                               </el-link>
                             </el-row>
                             <el-row>
@@ -129,18 +134,55 @@
                     </div>
                   </div>
                 </el-col>
-                <el-col :span="14">
+                <el-col :span="7">
                   <div class="container" style="margin:20px">
-                    <h2>最近登录的用户</h2>
+                    <h2>我们有这些进行中的活动</h2>
+                    <div>
+                      <el-row>
+                        <el-card :body-style="{ padding: '0px' }" style="margin:10px">
+                          <img
+                            src="https://gss3.bdstatic.com/-Po3dSag_xI4khGkpoWK1HF6hhy/baike/pic/item/b3119313b07eca800e79e2779f2397dda1448326.jpg"
+                            class="image"
+                          />
+                          <div style="padding: 14px;">
+                            <span>中华人民共和国成立70周年主题活动</span>
+                            <div class="bottom clearfix">
+                              <time class="time">{{ currentDate }}</time>
+                              <el-button type="text" class="button">加入我们，现在！</el-button>
+                            </div>
+                          </div>
+                        </el-card>
+                      </el-row>
+                      <el-row>
+                        <el-card :body-style="{ padding: '0px' }" style="margin:10px">
+                          <img
+                            src="https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1570100805380&di=37d29405a701b7f305ffa3358bff8b53&imgtype=0&src=http%3A%2F%2Fwww.juimg.com%2Ftuku%2Fyulantu%2F140402%2F330580-14040211215869.jpg"
+                            class="image"
+                          />
+                          <div style="padding: 14px;">
+                            <span>每天运动一小时，健康工作50年活动</span>
+                            <div class="bottom clearfix">
+                              <time class="time">{{ currentDate }}</time>
+                              <el-button type="text" class="button">加入我们，现在！</el-button>
+                            </div>
+                          </div>
+                        </el-card>
+                      </el-row>
+                    </div>
+                  </div>
+                </el-col>
+                <el-col :span="10">
+                  <div class="container" style="margin:20px">
+                    <h2>这些人，可能成为你的朋友</h2>
                     <div>
                       <el-row
-                        v-for="(i,index) in (Math.ceil((lastLoginUsers.length / 4)))"
+                        v-for="(i,index) in (Math.ceil((recommendFriends.length / 4)))"
                         :key="index"
                       >
                         <el-col
                           :span="6"
                           style="padding: 3px;"
-                          v-for="(item,index) in lastLoginUsers"
+                          v-for="(item,index) in recommendFriends"
                           v-show="(i - 1) * 4 <= index && index < i * 4"
                           :key="index"
                         >
@@ -148,10 +190,36 @@
                             <img :src="item.photo" class="image" />
                             <div style="padding: 2px;">
                               <span style="font-size:62.5%;">{{item.nickname}}</span>
-
+                              <br />
                               <span style="font-size:62.5%">{{item.university}}</span>
                               <br />
-                              <span style="font-size:62.5%">{{item.lastLoginAt}}</span>
+                              <el-link type="primary" style="font-size:40%;">详细信息</el-link>
+                            </div>
+                          </el-card>
+                        </el-col>
+                      </el-row>
+                    </div>
+                  </div>
+                  <div class="container" style="margin:20px">
+                    <h2>尝试加入这些组织</h2>
+                    <div>
+                      <el-row
+                        v-for="(i,index) in (Math.ceil((recommendOrganization.length / 4)))"
+                        :key="index"
+                      >
+                        <el-col
+                          :span="6"
+                          style="padding: 3px;"
+                          v-for="(item,index) in recommendOrganization"
+                          v-show="(i - 1) * 4 <= index && index < i * 4"
+                          :key="index"
+                        >
+                          <el-card :body-style="{ padding: '0px' }">
+                            <img :src="item.photo" class="image" />
+                            <div style="padding: 2px;">
+                              <span style="font-size:62.5%;">{{item.name}}</span>
+                              <br />
+                              <el-link type="primary" style="font-size:40%;">尝试加入</el-link>
                             </div>
                           </el-card>
                         </el-col>
@@ -260,7 +328,7 @@ export default {
       activeNames: "",
       url:
         "https://fuss10.elemecdn.com/e/5d/4a731a90594a4af544c0c25941171jpeg.jpeg",
-      lastLoginUsers: [
+      recommendFriends: [
         {
           user_id: 1,
           nickname: "金智媛",
@@ -268,22 +336,6 @@ export default {
           lastLoginAt: "14 sec ago",
           photo:
             "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1569063950234&di=f9e666fa5b2219e12cd1e0119b6a692d&imgtype=0&src=http%3A%2F%2Fimg4.duitang.com%2Fuploads%2Fitem%2F201406%2F03%2F20140603205522_ZirQE.thumb.600_0.jpeg"
-        },
-        {
-          user_id: 2,
-          nickname: "Yonna",
-          university: "北京大学",
-          lastLoginAt: "18 sec ago",
-          photo:
-            "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1569658749&di=d621e95842f713f897d1c9f9d14160bc&imgtype=jpg&er=1&src=http%3A%2F%2Fcdn.duitang.com%2Fuploads%2Fitem%2F201507%2F27%2F20150727151430_E5CS4.thumb.700_0.jpeg"
-        },
-        {
-          user_id: 3,
-          nickname: "Yonna",
-          university: "中南大学",
-          lastLoginAt: "24 sec ago",
-          photo:
-            "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1569218053597&di=00d61280e892d0f0fdf32eeda5ab1fcf&imgtype=0&src=http%3A%2F%2Fimg4q.duitang.com%2Fuploads%2Fitem%2F201411%2F20%2F20141120170055_cKStw.jpeg"
         },
         {
           user_id: 4,
@@ -294,28 +346,12 @@ export default {
             "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1569064081142&di=50668130821b13405f195658dc4506d4&imgtype=0&src=http%3A%2F%2Fcdn.duitang.com%2Fuploads%2Fitem%2F201407%2F24%2F20140724190906_MCkXs.thumb.700_0.jpeg"
         },
         {
-          user_id: 5,
-          nickname: "Yonna",
-          university: "中央民族大学",
-          lastLoginAt: "1 min 14 sec ago",
-          photo:
-            "https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=3284581089,709717092&fm=26&gp=0.jpg"
-        },
-        {
           user_id: 6,
           nickname: "Yonna",
           university: "武汉大学",
           lastLoginAt: "1 min 26 sec ago",
           photo:
             "https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=1743168392,2407869980&fm=26&gp=0.jpg"
-        },
-        {
-          user_id: 7,
-          nickname: "Yonna",
-          university: "延世大学",
-          lastLoginAt: "1 min 29 sec ago",
-          photo:
-            "https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=1392078492,1897050294&fm=26&gp=0.jpg"
         },
         {
           user_id: 8,
@@ -333,22 +369,7 @@ export default {
           photo:
             "https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=4176040192,3002869256&fm=26&gp=0.jpg"
         },
-        {
-          user_id: 10,
-          nickname: "Yonna",
-          university: "武汉大学",
-          lastLoginAt: "1 min 26 sec ago",
-          photo:
-            "https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=1476095006,2188158271&fm=26&gp=0.jpg"
-        },
-        {
-          user_id: 11,
-          nickname: "Yonna",
-          university: "延世大学",
-          lastLoginAt: "1 min 29 sec ago",
-          photo:
-            "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1569218725789&di=9c00b6dc70418ca1bf5bfcd6254e961c&imgtype=0&src=http%3A%2F%2Fb-ssl.duitang.com%2Fuploads%2Fitem%2F201806%2F21%2F20180621210646_mmltp.thumb.700_0.jpg"
-        },
+
         {
           user_id: 12,
           nickname: "Yonna",
@@ -356,22 +377,6 @@ export default {
           lastLoginAt: "1 min 34 sec ago",
           photo:
             "https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=1004963647,1607232271&fm=26&gp=0.jpg"
-        },
-        {
-          user_id: 13,
-          nickname: "Yonna",
-          university: "中央民族大学",
-          lastLoginAt: "1 min 14 sec ago",
-          photo:
-            "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1569218563776&di=2f13cfde907b4f7b48ac548d274b1143&imgtype=0&src=http%3A%2F%2Fb-ssl.duitang.com%2Fuploads%2Fitem%2F201504%2F09%2F20150409H4542_GVEAm.jpeg"
-        },
-        {
-          user_id: 14,
-          nickname: "Yonna",
-          university: "武汉大学",
-          lastLoginAt: "1 min 26 sec ago",
-          photo:
-            "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1569218563775&di=36090be434652bfefca531cac1466004&imgtype=0&src=http%3A%2F%2Fcdn.duitang.com%2Fuploads%2Fitem%2F201410%2F16%2F20141016202155_5ycRZ.thumb.700_0.jpeg"
         },
         {
           user_id: 15,
@@ -388,6 +393,32 @@ export default {
           lastLoginAt: "1 min 34 sec ago",
           photo:
             "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1569218725789&di=f3d2ffa1628b877d975d842a2f39a583&imgtype=0&src=http%3A%2F%2Fb-ssl.duitang.com%2Fuploads%2Fitem%2F201511%2F20%2F20151120171114_LkFSi.thumb.700_0.jpeg"
+        }
+      ],
+      recommendOrganization: [
+        {
+          group_id: 1,
+          name: "中国共产党",
+          photo:
+            "https://gss1.bdstatic.com/-vo3dSag_xI4khGkpoWK1HF6hhy/baike/pic/item/dbb44aed2e738bd4ee2f2e66aa8b87d6277ff95f.jpg"
+        },
+        {
+          group_id: 4,
+          name: "中国红十字基金会",
+          photo:
+            "https://gss1.bdstatic.com/-vo3dSag_xI4khGkpoWK1HF6hhy/baike/crop%3D0%2C149%2C1024%2C676%3Bc0%3Dbaike116%2C5%2C5%2C116%2C38/sign=17f0bac29b8fa0ec6b883e4d1ba775df/c9fcc3cec3fdfc03e7659b49de3f8794a4c22625.jpg"
+        },
+        {
+          group_id: 6,
+          name: "联合国儿童基金会",
+          photo:
+            "https://gss2.bdstatic.com/9fo3dSag_xI4khGkpoWK1HF6hhy/baike/c0%3Dbaike80%2C5%2C5%2C80%2C26/sign=af8df6b4e2dde711f3df4ba4c686a57e/a50f4bfbfbedab6479a00b71f036afc378311eb4.jpg"
+        },
+        {
+          group_id: 8,
+          name: "中国共青团",
+          photo:
+            "https://gss3.bdstatic.com/7Po3dSag_xI4khGkpoWK1HF6hhy/baike/pic/item/3812b31bb051f819b19cf0e7d4b44aed2f73e7e6.jpg"
         }
       ],
       searchConditions: {
